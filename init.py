0a57489fa2d32db467357057061dfc3328cf92d3
MaksKos <maks.kostrow@yandex.ru>
2022-04-17 23:30:07 +0300

    first commit

--- init.py
+++ init.py
@@ -1,1 +1,25 @@
+from tkinter import N
+import numpy as np
+
+n_cells = 1000
+time0 = 10*n_cells
+tau = 100
+precision = 0.5
+
+density = 0.05
+
+n_cars = int(n_cells*density)
+car_position = np.random.choice(n_cells-1, size=n_cars, replace=False)
+car_velosity = np.zeros(n_cars)
+road = np.zeros(n_cells)
+
+for index in car_position:
+    road[index]=1
+
+
+for _ in range(tau+time0):
+    distance = np.roll(car_position, -1)-car_position
+    distance[-1] += n_cells
+
+    
 
