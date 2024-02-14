03e1f0f1840aa141f70e97501bb75be571c120ab
MaksKos <maks.kostrow@yandex.ru>
2024-02-14 23:16:07 +0300

    real s-nfs

--- image_x_t.py
+++ image_x_t.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 n_cells = 1000
 time0 = 10*n_cells
 tau = 1000
+p = 0.5
 
 density = 0.4
-auto_car = 0
+auto_car = 0.5
 
 n_cars = int(n_cells*density)
 n_adr = int(n_cars*auto_car)
 
 model = NaShAuto(n_cells, n_cars-n_adr, n_adr)
+model.set_slow_probability(p)
 model.system_stabilization(time0)
 model.system_research(tau)
 
@@ -30,9 +32,11 @@ time = np.linspace(0, tau-1, tau, dtype=int)
 cells = np.linspace(0, n_cells-1, n_cells, dtype=int)
 grid = np.meshgrid(cells, time, indexing='xy')
 
+#plt.scatter(grid[0], grid[1], marker='s', c='grey', s=1, alpha=0.2)
+#bar_pic=plt.scatter(grid[0], grid[1], marker ='s', c = road_map, cmap = 'RdYlGn', s = 4, alpha = 1, edgecolors='black', linewidths=0.5 )
 bar_pic=plt.scatter(grid[0], grid[1], marker ='s', c = road_map, cmap = 'RdYlGn', s = 0.1, alpha = 1 )
-plt.xlim(0, 1000)
-plt.ylim(0, 1000)
+plt.xlim(0, n_cells)
+plt.ylim(0, tau)
 plt.gca().invert_yaxis()
 plt.colorbar()
 plt.clim(0, 5)
@@ -41,4 +45,4 @@ plt.xlabel(r'$\mathrm{cells}$',  fontsize = fs)
 plt.ylabel(r'$\mathrm{time}$',  fontsize = fs)
 #plt.grid(linewidth=0.5)
 plt.show()
-fig.savefig(f'data/x_t_{density}.jpg', dpi = 400,pad_inches=0.1, format='jpg', bbox_inches='tight')
+fig.savefig(f'data/x_t_{density}.jpg', dpi = 200, pad_inches=0.1, format='jpg', bbox_inches='tight')
--- modules/models.py
+++ modules/models.py
@@ -265,7 +265,7 @@ class StochasticNFS():
             self.matrix_velosity[step] = self.car_velosity
 
     def avarage_flow(self):
-        """Calculate avarage flow of model"""
+        """Calculate average flow of model"""
         if self.matrix_velosity is None:
             raise ValueError("Research system before calculate flow")
         return self.matrix_velosity.sum()/self.matrix_velosity.shape[0]/self.n_cells
@@ -275,7 +275,7 @@ class StochasticNFS():
         Method make matrix of road
         size [n_cells, time step research]
         if cell is empty -> None
-        if cell with cars -> value of car's velosity
+        if cell with cars -> value of car's velocity
         """
         if self.matrix_position is None or self.matrix_velosity is None:
             raise ValueError("Research system before calculate flow")
@@ -283,6 +283,169 @@ class StochasticNFS():
         for step in range(road.shape[0]):
             road[step][self.matrix_position[step]] = self.matrix_velosity[step]
         return road
+
+
+class RealisticNFS:
+
+    _velocity_max = 5
+    #  probability for slow to start effect
+    _q = 0.99
+    #  probability for S
+    _r = 0.99
+    _S = [1, 2]
+    #  probability for random breaking
+    _p = 0.96
+    _P1 = 0.999
+    _P2 = 0.99
+    _P3 = 0.98
+    _P4 = 0.01
+    _D = 15
+
+    def __init__(self, n_cells: int, n_cars: int, uniform=True) -> None:
+        """
+        n_cells: size of road in cells
+        n_cars: number of cars
+        uniform: <bool> uniform distribution of cars' position
+        """
+        if n_cars < 2:
+            raise ValueError("Minimum 2 cars should be get")
+        self.n_cells = n_cells
+        self.n_cars = n_cars
+        if uniform:
+            self.car_position = np.arange(n_cars) * (n_cells//n_cars)
+        else:
+            self.car_position = np.sort(np.random.choice(n_cells, size=n_cars, replace=False))
+        self.car_position_previous = self.car_position.copy()
+        self.car_velocity = np.zeros(n_cars, dtype=int)
+        # self.distance = np.zeros(n_cars, dtype=int)
+        self.v_min = np.zeros(n_cars, dtype=int)
+        self.stability = False
+        self.matrix_position = None
+        self.matrix_velocity = None
+
+    def step(self):
+        """Make one time step of model"""
+        #  step prepare
+        self.anticipation = np.random.choice(self._S, size=self.n_cars, p=[1-self._r, self._r])
+        i = np.arange(self.n_cars)
+        i_next = (i+self.anticipation) % self.n_cars
+        initial_velocity = self.car_velocity.copy()
+        #  d(i)
+        length = 1
+        distance = ((np.roll(self.car_position, -1)- self.car_position + self.n_cells)
+                    % self.n_cells - length)
+
+        #  rule 1 acceleration
+        #  d >= D and v(i) <= v(i+1)
+        distance_filter = distance >= self._D
+        velocity_filter = initial_velocity <= np.roll(initial_velocity, -1)
+        acceleration = (distance_filter * velocity_filter) # 0 or 1
+        self.car_velocity = np.min([self.v_max, self.car_velocity + acceleration], axis=0)
+
+        #  rule 2 slow-to-start effect
+        slow_start = np.random.choice([False, True], size=self.n_cars, p=[1-self._q, self._q])
+        distance_anticipation_previous = (
+                (self.car_position_previous.take(i_next) - self.car_position_previous + self.n_cells)
+                % self.n_cells - self.anticipation
+        )
+        if self.n_cars <= max(self._S):
+            distance_anticipation_previous[i_next == i] = self.n_cells
+        assert distance_anticipation_previous.min() >= 0
+        velocity = np.min([self.car_velocity*slow_start, distance_anticipation_previous*slow_start], axis=0)
+        self.car_velocity = np.max([self.car_velocity*np.invert(slow_start), velocity], axis=0)
+
+        #  rule 3 perspective effect
+        distance_anticipation = (
+                (self.car_position.take(i_next) - self.car_position + self.n_cells)
+                % self.n_cells - self.anticipation
+        )
+        if self.n_cars <= max(self._S):
+            distance_anticipation[i_next == i] = self.n_cells
+        assert distance_anticipation.min() >= 0
+        self.car_velocity = np.min([self.car_velocity, distance_anticipation], axis=0)
+
+        #  rule 4 random braking
+        min_available_velocity = 1
+        velocity_rule_4 = []
+        for vel_i, dist_i, vel_next_init, vel_init in zip(
+                self.car_velocity, distance,
+                np.roll(initial_velocity, -1),
+                initial_velocity,
+        ):
+            if vel_i <= min_available_velocity:
+                velocity_rule_4.append(vel_i)
+                continue
+            if dist_i >= self._D:
+                prob = self._P1
+            else:
+                if vel_init < vel_next_init:
+                    prob = self._P2
+                elif vel_init == vel_next_init:
+                    prob = self._P3
+                else:
+                    prob = self._P4
+
+            random_break = np.random.choice([0, 1], size=1, p=[prob, 1 - prob])
+            velocity_rule_4.append(vel_i-random_break)
+
+        self.car_velocity = np.array(velocity_rule_4)
+
+        #  rule 5 collision avoidance
+        self.car_velocity = np.min([self.car_velocity, np.roll(self.car_velocity, -1) + distance], axis=0)
+
+        #  rule 6 position renewing
+        self.car_position_previous = self.car_position.copy()
+        self.car_position += self.car_velocity.astype(int)
+        self.car_position %= self.n_cells
+
+    def set_max_velocity(self, v_max: int) -> None:
+        self._velocity_max = v_max
+        self.v_max = np.full(self.n_cars, self._velocity_max, dtype=int)
+
+    def system_stabilization(self, n_step:int):
+        """
+        Stabilizes system
+        n_step: time step for stabilization
+        Recommend choose n_step = 10*n_cells
+        """
+        for _ in range(n_step):
+            self.step()
+        self.stability = True
+
+    def system_research(self, n_step:int):
+        """
+        Method save information about cars position and velocity
+        n_step: time step for system research
+        """
+        if not self.stability:
+            print("Model hasn't stabilizes")
+        self.matrix_position = np.zeros((n_step, self.n_cars), dtype=int)
+        self.matrix_velocity = np.zeros((n_step, self.n_cars), dtype=int)
+        for step in range(n_step):
+            self.matrix_position[step] = self.car_position
+            self.step()
+            self.matrix_velocity[step] = self.car_velocity
+
+    def average_flow(self):
+        """Calculate average flow of model"""
+        if self.matrix_velocity is None:
+            raise ValueError("Research system before calculate flow")
+        return self.matrix_velocity.sum()/self.matrix_velocity.shape[0]/self.n_cells
+
+    def diagram_x_t(self):
+        """
+        Method make matrix of road
+        size [n_cells, time step research]
+        if cell is empty -> None
+        if cell with cars -> value of car's velocity
+        """
+        if self.matrix_position is None or self.matrix_velocity is None:
+            raise ValueError("Research system before calculate flow")
+        road = np.full((self.matrix_position.shape[0], self.n_cells), np.nan)
+        for step in range(road.shape[0]):
+            road[step][self.matrix_position[step]] = self.matrix_velocity[step]
+        return road
+
 
 def get_flow(n_cars, n_cells, prob, time_s, time_r):
     """
@@ -299,6 +462,7 @@ def get_flow(n_cars, n_cells, prob, time_s, time_r):
     model.system_research(time_r)
     return model.avarage_flow()
 
+
 def get_flow_auto(n_cars, n_adr, n_cells, prob, time_s, time_r):
     """
     Function for calculate flow of model
@@ -314,6 +478,7 @@ def get_flow_auto(n_cars, n_adr, n_cells, prob, time_s, time_r):
     model.system_stabilization(time_s)
     model.system_research(time_r)
     return model.avarage_flow()
+
 
 def get_flow_S_NFS(n_cars, n_cells, prob, time_s, time_r, v_max=5, uniform=True):
     """
@@ -333,3 +498,23 @@ def get_flow_S_NFS(n_cars, n_cells, prob, time_s, time_r, v_max=5, uniform=True)
     model.system_stabilization(time_s)
     model.system_research(time_r)
     return model.avarage_flow()
+
+
+def get_flow_s_nfs_real(n_cars, n_cells, time_s, time_r, v_max=5, uniform=True):
+    """
+    Function for calculate flow of model
+    n_cars: number of cars
+    n_cells: size of road in cells
+    prob: probability <dict>
+    time_s: time step for stabilize model
+    time_r: time step for research model
+    """
+    if n_cars < 1:
+        return 0
+    if n_cars == 1:
+        return v_max/n_cells
+    model = RealisticNFS(n_cells, n_cars, uniform)
+    model.set_max_velocity(v_max)
+    model.system_stabilization(time_s)
+    model.system_research(time_r)
+    return model.average_flow()
